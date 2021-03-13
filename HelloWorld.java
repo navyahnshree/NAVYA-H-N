0a43c347e77cb426654f2cc120107957d69987d7
Navyahnshree <navyahnshree@gmail.com>
2021-03-13 19:23:29 +0530

    Basic Program

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+*Name:Navya H Name
+*Description: Compilation and Running Test
+*Date: 13/3/2021
+*/
+
+class HelloWorld{
+public static void main(String [] args){
+System.out.println("Hello World");
+}
+}
